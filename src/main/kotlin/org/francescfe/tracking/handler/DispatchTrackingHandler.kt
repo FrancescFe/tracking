fb7fb94fe3e9205202dcbe7793ebcc18688d4b07
FrancescFe <fraferru@gmail.com>
2026-03-27 12:56:30 +0100

    feat(001): remove unneeded service

--- src/main/kotlin/org/francescfe/tracking/handler/DispatchTrackingHandler.kt
+++ src/main/kotlin/org/francescfe/tracking/handler/DispatchTrackingHandler.kt
@@ -1,15 +1,12 @@
 package org.francescfe.tracking.handler
 
 import org.francescfe.tracking.message.DispatchTracking
-import org.francescfe.tracking.service.TrackingService
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
-class DispatchTrackingHandler(
-    private val trackingService: TrackingService,
-) {
+class DispatchTrackingHandler {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
@@ -21,6 +18,5 @@ class DispatchTrackingHandler(
     )
     fun listen(payload: DispatchTracking) {
         log.info("Received dispatch tracking payload: {}", payload)
-        trackingService.process(payload)
     }
 }
